63845dea26740adf36267a644c12c3b735d7aed2
xiongxiaoyang <773861846@qq.com>
2020-12-17 15:09:55 +0800

    删除后台代码

--- novel-admin/src/main/java/com/java2nb/common/utils/Base64Utils.java
+++
@@ -1,5 +0,0 @@
-package com.java2nb.common.utils;
-
-public class Base64Utils {
-	
-}
--- novel-admin/src/main/java/com/java2nb/common/utils/StringUtils.java
+++
@@ -1,7 +0,0 @@
-package com.java2nb.common.utils;
-
-/**
- * @author xiongxy
- */
-public class StringUtils extends org.apache.commons.lang3.StringUtils{
-}
